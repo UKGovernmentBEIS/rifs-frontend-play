8f576b760a45a74f460d38989b7bbf76d723e3a8
Doug Clinton <doug@wiredthing.com>
2016-10-19 11:03:55 +0100

    Moved the formatting of the application completed date into a method on Application

--- src/main/scala/models/Application.scala
+++ src/main/scala/models/Application.scala
@@ -1,6 +1,7 @@
 package models
 
 import org.joda.time.LocalDateTime
+import org.joda.time.format.DateTimeFormat
 import play.api.libs.json.JsObject
 
 case class ApplicationId(id: Long) extends AnyVal
@@ -9,7 +10,11 @@ case class ApplicationSectionId(id: Long) extends AnyVal
 
 case class Application(id: ApplicationId, applicationFormId: ApplicationFormId)
 
-case class ApplicationSection(id: ApplicationSectionId, applicationId: ApplicationId, sectionNumber: Int, answers: JsObject, completedAt: Option[LocalDateTime])
+case class ApplicationSection(id: ApplicationSectionId, applicationId: ApplicationId, sectionNumber: Int, answers: JsObject, completedAt: Option[LocalDateTime]) {
+  val dtf = DateTimeFormat.forPattern("d MMMM HH:mm")
+
+  def completedAtText: Option[String] = completedAt.map(d => s"Completed ${dtf.print(d)}")
+}
 
 case class ApplicationSectionOverview(sectionNumber: Int, status: String)
 
