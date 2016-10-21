09ca72a14b1551510eece4e2cf35bf899fe04f48
Venkata Mutyala <venkata.mutyala@triad.co.uk>
2016-10-21 16:30:08 +0100

    1117 changes

--- src/main/scala/controllers/ApplicationPreviewController.scala
+++ src/main/scala/controllers/ApplicationPreviewController.scala
@@ -16,38 +16,8 @@ class ApplicationPreviewController @Inject()(applications: ApplicationOps, appli
   extends Controller
     with ControllerUtils {
 
- /* def showOrCreateForForm(id: ApplicationFormId) = Action.async {
-    applications.getOrCreateForForm(id).map {
-      case Some(app) => Redirect(controllers.routes.ApplicationController.show(app.id))
-      case None => NotFound
-    }
-  }
-
-  def show(id: ApplicationId) = Action.async {
-    val t = for {
-      a <- OptionT(applications.overview(id))
-      af <- OptionT(applicationForms.byId(a.applicationFormId))
-    } yield (af, a)
-
-    t.value.map {
-      case Some((form, overview)) => Ok(views.html.showApplicationForm(form, overview))
-      case None => NotFound
-    }
-  }
-
-  val rules: Map[String, Seq[FieldRule]] = Map("title" -> Seq(WordCountRule(20), MandatoryRule))
-
-  type FieldErrors = Map[String, NonEmptyList[String]]
-  val noErrors: FieldErrors = Map()*/
-
   def previewSectionForm(id: ApplicationId, sectionNumber: Int) = Action.async { request =>
     if (sectionNumber == 1) applications.getSection(id, sectionNumber).flatMap { section =>
-      /*val doValidation = request.flash.get("doValidation").exists(_ => true)
-
-      val errs: FieldErrors = section.map { s =>
-        if (doValidation) validate(s.answers, rules) else noErrors
-      }.getOrElse(noErrors)
-      */
       previewTitle(id, section)
     }
     else Future.successful(Ok(views.html.wip(routes.ApplicationController.show(id).url)))
@@ -67,49 +37,4 @@ class ApplicationPreviewController @Inject()(applications: ApplicationOps, appli
     }
   }
 
-
-  /**
-    * Note if more than one button action name is present in the keys then it is indeterminate as to
-    * which one will be returned. This shouldn't occur if the form is properly submitted from a
-    * browser, though.
-    */
-  /*def decodeButton(keys: Set[String]): Option[ButtonAction] = keys.flatMap(ButtonAction.unapply).headOption
-
-  def postSection(id: ApplicationId, sectionNumber: Int) = Action.async(parse.urlFormEncoded) { implicit request =>
-    // Drop keys that start with '_' as these are "system" keys like the button name
-    val jsonFormValues = formToJson(request.body.filterKeys(k => !k.startsWith("_")))
-    val button: Option[ButtonAction] = decodeButton(request.body.keySet)
-
-    takeAction(id, sectionNumber, button, jsonFormValues)
-  }
-
-  def takeAction(id: ApplicationId, sectionNumber: Int, button: Option[ButtonAction], fieldValues: JsObject): Future[Result] = {
-    button.map {
-      case Complete =>
-        val errs = validate(fieldValues, rules)
-        if (errs.keySet.isEmpty) {
-          applications.completeSection(id, sectionNumber, fieldValues).map { _ =>
-            Redirect(routes.ApplicationController.show(id))
-          }
-        } else {
-          applications.saveSection(id, sectionNumber, fieldValues).map { _ =>
-            Redirect(routes.ApplicationController.showSectionForm(id, sectionNumber)).flashing(("doValidation", "true"))
-          }
-        }
-      case Save =>
-        applications.saveSection(id, sectionNumber, fieldValues)
-        Future.successful(Redirect(routes.ApplicationController.show(id)))
-      case Preview => Future.successful(Redirect(routes.ApplicationController.show(id)))
-    }.getOrElse(Future.successful(BadRequest))
-  }
-
-  def validate(fieldValues: JsObject, rules: Map[String, Seq[FieldRule]]): Map[String, NonEmptyList[String]] = {
-    rules.map { case (fieldName, rs) =>
-      fieldName -> (fieldValues \ fieldName match {
-        case JsDefined(JsString(s)) => NonEmptyList.fromList(rs.flatMap(r => r.validate(s)).toList)
-        case _ => if (rs.contains(MandatoryRule)) NonEmptyList.fromList(MandatoryRule.validate("").toList) else None
-      })
-    }.collect { case (fieldName, Some(errs)) => fieldName -> errs }
-  }
-  */
 }
