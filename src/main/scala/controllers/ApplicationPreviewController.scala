905c580b644f0327e5bd6892fca1bf383e482774
Doug Clinton <doug@wiredthing.com>
2016-10-26 10:33:19 +0100

    Re-named `previewTitle` to `renderSectionPreview` in `ApplicationPreviewController`

--- src/main/scala/controllers/ApplicationPreviewController.scala
+++ src/main/scala/controllers/ApplicationPreviewController.scala
@@ -6,7 +6,6 @@ import cats.data.OptionT
 import cats.instances.future._
 import forms.Field
 import models.{ApplicationId, ApplicationSection}
-import play.api.Logger
 import play.api.libs.json.JsObject
 import play.api.mvc.{Action, Controller}
 import services.{ApplicationFormOps, ApplicationOps, OpportunityOps}
@@ -24,13 +23,13 @@ class ApplicationPreviewController @Inject()(applications: ApplicationOps, appli
       case Some(fields) => applications.getSection(id, sectionNumber).flatMap { section =>
         val populatedFields = fields.map { f => f.withValuesFrom(section.map(_.answers).getOrElse(JsObject(Seq()))) }
 
-        previewTitle(id, sectionNumber, section, populatedFields)
+        renderSectionPreview(id, sectionNumber, section, populatedFields)
       }
       case None => Future.successful(Ok(views.html.wip(routes.ApplicationController.show(id).url)))
     }
   }
 
-  def previewTitle(id: ApplicationId, sectionNumber: Int, section: Option[ApplicationSection], fields: Seq[Field]) = {
+  def renderSectionPreview(id: ApplicationId, sectionNumber: Int, section: Option[ApplicationSection], fields: Seq[Field]) = {
     val ft = for {
       a <- OptionT(applications.byId(id))
       af <- OptionT(applicationForms.byId(a.applicationFormId))
