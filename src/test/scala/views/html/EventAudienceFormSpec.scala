607064878810998c2692969db9e76acf772945bf
BenKDishman <ben.dishman@digital.bis.gov.uk>
2016-10-27 15:32:09 +0100

    Added rudimentary tests

--- src/test/scala/views/html/EventAudienceFormSpec.scala
+++ src/test/scala/views/html/EventAudienceFormSpec.scala
@@ -7,7 +7,7 @@ import org.scalatest.{Matchers, OptionValues, WordSpecLike}
 import play.api.libs.json.JsObject
 import play.twirl.api.Html
 
-class TargetAudienceFormSpec extends WordSpecLike with Matchers with OptionValues {
+class EventAudienceFormSpec extends WordSpecLike with Matchers with OptionValues {
 
   "sectionForm" should {
 
@@ -16,10 +16,10 @@ class TargetAudienceFormSpec extends WordSpecLike with Matchers with OptionValue
       val soup = Jsoup.parse(html.toString())
       Option(soup.getElementById("title_help_text")).value.text() shouldBe "500 words maximum"
     }
-    "Event Objective Help text" in {
+    "Event Audience Help text" in {
       val html: Html = generatePage(None)
       val soup = Jsoup.parse(html.toString())
-      Option(soup.getElementsByTag("h4")).value.text() shouldBe "What are the objectives of the event?"
+      Option(soup.getElementsByTag("h4")).value.text() shouldBe "Who is the event’s target audience?"
     }
   }
 
@@ -29,9 +29,9 @@ class TargetAudienceFormSpec extends WordSpecLike with Matchers with OptionValue
     sectionForm(
       Application(ApplicationId(1), ApplicationFormId(1)),
       section,
-      ApplicationFormSection(3, "Event Objectives"),
+      ApplicationFormSection(5, "Event Audience"),
       Opportunity(OpportunityId(1), "Research priorities in health care", "", None, OpportunityValue(0, ""), Seq()),
-      Seq(TextAreaField(Some("label"), "eventObjectives", Seq(WordCountRule(500)), None, None, Option(Question("What are the objectives of the event?", Option(""), Option("Help Text")))).withValuesFrom(values.getOrElse(JsObject(Seq()))))
+      Seq(TextAreaField(Some("label"), "eventAudience", Seq(WordCountRule(500)), None, None, Option(Question("Who is the event’s target audience?", Option(""), Option("Help Text")))).withValuesFrom(values.getOrElse(JsObject(Seq()))))
     )
   }
 }
