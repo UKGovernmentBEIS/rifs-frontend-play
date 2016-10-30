94a7ca2f6b42d4f5916acc85385fb315557b9243
Doug Clinton <doug@wellfactored.com>
2016-10-30 12:15:40 +0000

    Fixed a bug in the MandatoryValidator error message generating code

--- src/main/scala/forms/validation/MandatoryValidator.scala
+++ src/main/scala/forms/validation/MandatoryValidator.scala
@@ -7,7 +7,7 @@ case class MandatoryValidator(displayName: Option[String] = None) extends FieldV
   override def normalise(os: Option[String]): Option[String] = os.map(_.trim())
 
   override def validate(path: String, s: Option[String]): ValidatedNel[FieldError, String] = {
-    val fieldName = displayName.map(n => "'$n'").getOrElse("Field")
+    val fieldName = displayName.map(n => s"'$n'").getOrElse("Field")
     normalise(s) match {
       case None | Some("") => FieldError(path, s"$fieldName cannot be empty").invalidNel
       case Some(n) => n.validNel
