614d6c60e2c5e4b6a289f5b265654a5fb60502bc
the-daniel-rothig <daniel.rothig@gmail.com>
2016-10-31 10:45:03 +0000

    some refactoring to make the code cleaner

--- src/main/public/javascripts/application-rules.js
+++ src/main/public/javascripts/application-rules.js
@@ -1,15 +1,21 @@
 (function() {
+    if(!document.getElementsByClassName || !document.body.addEventListener) {return;}
+
     var rules = {
         "WordCountRule"(value, config) {
              var trimmed = value.replace(/^\s+|\s+$/gm,"");
-             if(!trimmed) {return "" + config.maxWords + " " + (config.maxWords === 1 ? "word" : "words") + " maximum";}
              var w = trimmed.split(/\s+/).length;
-             if (w <= config.maxWords) {return "Words remaining: " + (config.maxWords - w);}
-             return "" + (w - config.maxWords) + " " + (w - config.maxWords === 1 ? "word" : "words") + " over limit";
+             var words = function(count) {return count === 1 ? "word" : "words";}
+
+             return !trimmed ? "" + config.maxWords + " " + words(config.maxWords) + " maximum"
+                  : w <= config.maxWords ? "Words remaining: " + (config.maxWords - w)
+                  : "" + (w - config.maxWords) + " " + words(w - config.maxWords) + " over limit";
         }
     };
 
     function addInputListener(input, helper, rule, config) {
+        if (!input || !rule) {return;}
+
         input.addEventListener("keyup", function() {
             var output = rule(input.value, config);
             helper.innerHTML = output;
@@ -17,17 +23,13 @@
     }
 
     function rifsHelperText() {
-        if(!document.getElementsByClassName || !document.body.addEventListener) {return;}
-
         var helpers = document.getElementsByClassName("helptext");
-        if (helpers.length === 0) {return;}
         for (var i = 0; i < helpers.length; i++) {
             var helper = helpers[i];
             var input = document.getElementById(helper.getAttribute("data-for")),
                 rule = rules[helper.getAttribute("data-rule")] || null,
                 config = JSON.parse(helper.getAttribute("data-ruleconfig") || "{}");
 
-            if (!input || !rule) {continue;}
             addInputListener(input, helper, rule, config);
         }
     }
