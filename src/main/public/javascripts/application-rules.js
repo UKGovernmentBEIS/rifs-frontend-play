87c9dc8035e6f756b35294a9de8daad7e9530b42
the-daniel-rothig <daniel.rothig@gmail.com>
2016-10-31 09:44:12 +0000

    (some) code cleanup to keep codacy quieter

--- src/main/public/javascripts/application-rules.js
+++ src/main/public/javascripts/application-rules.js
@@ -1,26 +1,26 @@
 (function() {
     var rules = {
-        "WordCountRule": function(value, config) {
-             var trimmed = value.replace(/^\s+|\s+$/gm,'');
+        "WordCountRule"(value, config) {
+             var trimmed = value.replace(/^\s+|\s+$/gm,"");
              if(!trimmed) {return "" + config.maxWords + " " + (config.maxWords === 1 ? "word" : "words") + " maximum";}
              var w = trimmed.split(/\s+/).length;
              if (w <= config.maxWords) {return "Words remaining: " + (config.maxWords - w);}
              return "" + (w - config.maxWords) + " " + (w - config.maxWords === 1 ? "word" : "words") + " over limit";
         }
-    }
+    };
 
     function addInputListener(input, helper, rule, config) {
         input.addEventListener("keyup", function() {
             var output = rule(input.value, config);
             helper.innerHTML = output;
-        })
+        });
     }
 
     function rifsHelperText() {
         if(!document.getElementsByClassName || !document.body.addEventListener) {return;}
 
         var helpers = document.getElementsByClassName("helptext");
-        if (helpers.length == 0) return;
+        if (helpers.length === 0) {return;}
         for (var i = 0; i < helpers.length; i++) {
             var helper = helpers[i];
             var input = document.getElementById(helper.getAttribute("data-for")),
--- src/test/js/application-rules.js
+++ src/test/js/application-rules.js
@@ -1,30 +1,12 @@
-// Boundary tests
-testWordCountHelper("50 words maximum", "");
-testWordCountHelper("Words remaining: 46", "These are four words");
-testWordCountHelper("Words remaining: 1", repeat("word ", 49));
-testWordCountHelper("Words remaining: 0", repeat("word ", 50));
-testWordCountHelper("1 word over limit", repeat("word ", 51));
-testWordCountHelper("10 words over limit", repeat("word ", 60));
-
-//whitespace tolerance
-testWordCountHelper("Words remaining: 46", " These are four words");
-testWordCountHelper("Words remaining: 46", "These are four words  ");
-testWordCountHelper("Words remaining: 46", "\nThese are four words.....&hellip;  ");
-testWordCountHelper("Words remaining: 46", "These are four words.....still");
-
-//UTF
-testWordCountHelper("Words remaining: 46", "These are föur wörds");
-
-
 function testWordCountHelper(message, value) {
     QUnit.test("message for '" + value + "': " + message, function(assert) {
-      document.getElementById('qunit-fixture').innerHTML = "<input type='text' id='test'><span class='helptext' id='testhelptext' data-for='test' data-rule='WordCountRule' data-ruleconfig='{\"maxWords\":50}'></span>";
+      document.getElementById("qunit-fixture").innerHTML = "<input type='text' id='test'><span class='helptext' id='testhelptext' data-for='test' data-rule='WordCountRule' data-ruleconfig='{\"maxWords\":50}'></span>";
       window.rifsHelperText();
 
-      document.getElementById('test').value = value;
-      triggerEvent(document.getElementById('test'), "keyup");
-      assert.equal(document.getElementById('testhelptext').innerHTML, message)
-    })
+      document.getElementById("test").value = value;
+      triggerEvent(document.getElementById("test"), "keyup");
+      assert.equal(document.getElementById("testhelptext").innerHTML, message);
+    });
 }
 
 function triggerEvent(element, eventName) {
@@ -49,6 +31,27 @@ function triggerEvent(element, eventName) {
 
 function repeat(word, times) {
     var rtn = "";
-    for (var i =0; i < times; i++) rtn += word;
+    for (var i =0; i < times; i++) {rtn += word;}
     return rtn;
 }
+
+
+
+
+// Boundary tests
+testWordCountHelper("50 words maximum", "");
+testWordCountHelper("Words remaining: 46", "These are four words");
+testWordCountHelper("Words remaining: 1", repeat("word ", 49));
+testWordCountHelper("Words remaining: 0", repeat("word ", 50));
+testWordCountHelper("1 word over limit", repeat("word ", 51));
+testWordCountHelper("10 words over limit", repeat("word ", 60));
+
+//whitespace tolerance
+testWordCountHelper("Words remaining: 46", " These are four words");
+testWordCountHelper("Words remaining: 46", "These are four words  ");
+testWordCountHelper("Words remaining: 46", "\nThese are four words.....&hellip;  ");
+testWordCountHelper("Words remaining: 46", "These are four words.....still");
+
+//UTF
+testWordCountHelper("Words remaining: 46", "These are föur wörds");
+
