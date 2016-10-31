81a55d9bf11afef4c2d3413234c30364c4af9ea6
Doug Clinton <doug@wiredthing.com>
2016-10-31 16:16:58 +0000

    Improved the rendering of the form and got the field names right

--- src/main/scala/forms/CostItemField.scala
+++ src/main/scala/forms/CostItemField.scala
@@ -6,10 +6,10 @@ import play.twirl.api.Html
 
 case class CostItemField(name: String) extends Field {
 
-  val itemNameField = TextField(Some("Item"), "itemName", isNumeric = false)
-  val costField = CurrencyField(Some("Cost"), "cost")
-  val percentageField = TextField(Some("% from RC"), "percentage", isNumeric = true)
-  val justificationField = TextAreaField(Some("Justification of item"), "justification")
+  val itemNameField = TextField(Some("Item"), s"$name.itemName", isNumeric = false)
+  val costField = CurrencyField(Some("Cost"), s"$name.cost")
+  val percentageField = TextField(Some("% from RC"), s"$name.percentage", isNumeric = true)
+  val justificationField = TextAreaField(Some("Justification of item"), s"$name.justification")
 
   override def renderFormInput(questions: Map[String, Question], answers: Map[String, String], errs: Seq[FieldError], hints: Seq[FieldHint]): Html =
     views.html.renderers.costItemField(this, questions, answers, errs, hints)
