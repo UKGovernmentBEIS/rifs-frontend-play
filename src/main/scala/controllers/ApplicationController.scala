fe3123105474313ee19bfacb9ac116f9cdec4004
Doug Clinton <doug@wiredthing.com>
2016-11-01 11:16:10 +0000

    I've change the way that error handling works in the ApplicationController. If a form has errors (on Complete, SaveItem or Preview) then it is redisplayed immediately with the entered values rather than the browser being redirected back to the form. Those values are not, however, saved in the database. Invalid values are only saved if the user select "Save and Continue".
    I've also added an `id` attribute to the links to the sections on the Application Overview form to allow more robust lookup in the selenium tests.

--- src/main/scala/controllers/ApplicationController.scala
+++ src/main/scala/controllers/ApplicationController.scala
@@ -6,7 +6,6 @@ import cats.data.OptionT
 import cats.instances.future._
 import forms._
 import models._
-import play.api.Logger
 import play.api.libs.json._
 import play.api.mvc.{Action, Controller, Result}
 import services.{ApplicationFormOps, ApplicationOps, OpportunityOps}
@@ -40,18 +39,13 @@ class ApplicationController @Inject()(applications: ApplicationOps, applicationF
   import FieldCheckHelpers._
 
   def showSectionForm(id: ApplicationId, sectionNumber: Int) = Action.async { request =>
+    doShowSectionForm(id, sectionNumber)
+  }
+
+  def doShowSectionForm(id: ApplicationId, sectionNumber: Int, errs: FieldErrors = noErrors): Future[Result] = {
     fieldsFor(sectionNumber) match {
       case Some(fields) =>
         applications.getSection(id, sectionNumber).flatMap { section =>
-          val doValidation = request.flash.get("doValidation").exists(_ => true)
-          val doPreviewValidation = request.flash.get("doPreviewValidation").exists(_ => true)
-
-          val errs: FieldErrors = section.map { s =>
-            if (doValidation) check(s.answers, checksFor(sectionNumber))
-            else if (doPreviewValidation) check(s.answers, previewChecksFor(sectionNumber))
-            else noErrors
-          }.getOrElse(noErrors)
-
           val hints = section.map(s => hinting(s.answers, checksFor(sectionNumber))).getOrElse(List())
           renderSectionForm(id, sectionNumber, section, questionsFor(sectionNumber), fields, errs, hints)
         }
@@ -87,46 +81,81 @@ class ApplicationController @Inject()(applications: ApplicationOps, applicationF
     }
   }
 
+  def redisplaySectionForm(id: ApplicationId, sectionNumber: Int, answers: Map[String, String], errs: FieldErrors = noErrors): Future[Result] = {
+    val ft = for {
+      a <- OptionT(applications.overview(id))
+      af <- OptionT(applicationForms.byId(a.applicationFormId))
+      o <- OptionT(opportunities.byId(af.opportunityId))
+    } yield (a, af, o)
+
+    val sectionF = applications.getSection(id, sectionNumber)
+
+    fieldsFor(sectionNumber) match {
+      case Some(fields) =>
+        val questions = questionsFor(sectionNumber)
+        val y = for {
+          x <- ft.value
+          section <- sectionF
+        } yield (x, section)
+
+        y.map {
+          case (Some((app, appForm, opp)), section) =>
+            val formSection: ApplicationFormSection = appForm.sections.find(_.sectionNumber == sectionNumber).get
+            val hints = section.map(s => hinting(s.answers, checksFor(sectionNumber))).getOrElse(List())
+
+            sectionTypeFor(sectionNumber) match {
+              case VanillaSection => Ok(views.html.sectionForm(app, appForm, section, formSection, opp, fields, questions, answers, errs, hints))
+              case CostSection => Ok(views.html.costSectionForm(app, appForm, section, formSection, opp, fields, questions, answers, errs, hints))
+            }
+          case (None, _) => NotFound
+        }
+
+      case None => Future.successful(Ok(views.html.wip(routes.ApplicationController.show(id).url)))
+    }
+  }
+
   def postSection(id: ApplicationId, sectionNumber: Int) = Action.async(JsonForm.parser) { implicit request =>
     takeAction(id, sectionNumber, request.body.action, request.body.values)
   }
 
   def takeAction(id: ApplicationId, sectionNumber: Int, button: ButtonAction, fieldValues: JsObject): Future[Result] = {
     button match {
-      case Complete =>
-        val errs = check(fieldValues, checksFor(sectionNumber))
-        if (errs.isEmpty) {
-          applications.completeSection(id, sectionNumber, fieldValues).map { _ =>
-            Redirect(routes.ApplicationController.show(id))
-          }
-        } else {
-          applications.saveSection(id, sectionNumber, fieldValues).map { _ =>
-            Redirect(routes.ApplicationController.showSectionForm(id, sectionNumber)).flashing(("doValidation", "true"))
-          }
-        }
-      case Save =>
-        applications.saveSection(id, sectionNumber, fieldValues).map { _ =>
-          Redirect(routes.ApplicationController.show(id))
-        }
-      case SaveItem =>
-        // Save if there are no errors, otherwise redisplay with errors
-        val errs = check(fieldValues, checksFor(sectionNumber))
-        if (errs.isEmpty) {
-          applications.saveSection(id, sectionNumber, fieldValues).map { _ =>
-            Redirect(routes.ApplicationController.show(id))
-          }
-        } else {
-          Future.successful(Redirect(routes.ApplicationController.showSectionForm(id, sectionNumber)).flashing(("doValidation", "true")))
-        }
-      case Preview =>
-        applications.saveSection(id, sectionNumber, fieldValues).map { _ =>
-          val errs = check(fieldValues, previewChecksFor(sectionNumber))
-          if (errs.isEmpty) {
-            Redirect(routes.ApplicationPreviewController.previewSection(id, sectionNumber))
-          } else {
-            Redirect(routes.ApplicationController.showSectionForm(id, sectionNumber)).flashing(("doPreviewValidation", "true"))
-          }
-        }
+      case Complete => doComplete(id, sectionNumber, fieldValues)
+      case Save => doSave(id, sectionNumber, fieldValues)
+      case SaveItem => doSaveItem(id, sectionNumber, fieldValues)
+      case Preview => doPreview(id, sectionNumber, fieldValues)
     }
+  }
+
+  def doSave(id: ApplicationId, sectionNumber: Int, fieldValues: JsObject): Future[Result] =
+    applications.saveSection(id, sectionNumber, fieldValues).map { _ =>
+      Redirect(routes.ApplicationController.show(id))
+    }
+
+  def doComplete(id: ApplicationId, sectionNumber: Int, fieldValues: JsObject): Future[Result] = {
+    val errs = check(fieldValues, checksFor(sectionNumber))
+    if (errs.isEmpty) {
+      applications.completeSection(id, sectionNumber, fieldValues).map { _ =>
+        Redirect(routes.ApplicationController.show(id))
+      }
+    } else redisplaySectionForm(id, sectionNumber, JsonHelpers.flatten("", fieldValues), errs)
+  }
+
+  def doPreview(id: ApplicationId, sectionNumber: Int, fieldValues: JsObject): Future[Result] = {
+    val errs = check(fieldValues, previewChecksFor(sectionNumber))
+    if (errs.isEmpty) {
+      applications.saveSection(id, sectionNumber, fieldValues).map { _ =>
+        Redirect(routes.ApplicationPreviewController.previewSection(id, sectionNumber))
+      }
+    } else redisplaySectionForm(id, sectionNumber, JsonHelpers.flatten("", fieldValues), errs)
+  }
+
+
+  def doSaveItem(id: ApplicationId, sectionNumber: Int, fieldValues: JsObject): Future[Result] = {
+    // Save if there are no errors, otherwise redisplay with errors
+    val errs = check(fieldValues, checksFor(sectionNumber))
+    if (errs.isEmpty) {
+      doSave(id, sectionNumber, fieldValues)
+    } else redisplaySectionForm(id, sectionNumber, JsonHelpers.flatten("", fieldValues), errs)
   }
 }
