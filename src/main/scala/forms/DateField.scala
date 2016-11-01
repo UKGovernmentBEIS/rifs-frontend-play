52518e55c3472b1eaf843473a352e3f99f3b0c7e
the-daniel-rothig <daniel.rothig@gmail.com>
2016-11-01 13:55:02 +0000

    Accessible datestrings in aria-labels

--- src/main/scala/forms/DateField.scala
+++ src/main/scala/forms/DateField.scala
@@ -1,17 +1,33 @@
 package forms
 
-import forms.validation.{FieldError, FieldHint}
+import forms.validation.{FieldError, FieldHint, DateFieldValidator}
 import models.Question
+import org.joda.time.format._
+import org.joda.time.LocalDate
 import play.api.Logger
 import play.twirl.api.Html
 
 case class DateValues(day: Option[String], month: Option[String], year: Option[String])
 
-case class DateField(name: String) extends Field {
+case class DateField(name: String, validator: DateFieldValidator) extends Field {
   override def renderFormInput(questions: Map[String, Question], answers: Map[String, String], errs: Seq[FieldError], hints:Seq[FieldHint]): Html = {
     Logger.debug(s"rendering date field with $name using $answers and $errs")
     views.html.renderers.dateField(this, questions, answers, errs)
   }
 
-  override def renderPreview(answers: Map[String, String]): Html = views.html.renderers.preview.dateField(this, answers)
+  val fmt = DateTimeFormat.forPattern("d MMMM yyyy")
+  val accessFmt = AccessibleDateTimeFormat()
+
+  override def renderPreview(answers: Map[String, String]): Html = {
+    val day = answers.get(s"$name.day")
+    val month = answers.get(s"$name.month")
+    val year = answers.get(s"$name.year")
+    validator.validate("", DateValues(day,month,year)).map { date =>
+      views.html.renderers.preview.dateField(fmt.print(date), accessFmt.print(date))
+    }.leftMap { errs =>
+      Logger.debug(errs.toString())
+      // TODO: we rely on only being called with valid answers, but what if they're not?
+      views.html.renderers.preview.dateField("", "")
+    }.fold(identity, identity)
+  }
 }
--- src/main/scala/forms/DateWithDaysField.scala
+++ src/main/scala/forms/DateWithDaysField.scala
@@ -3,15 +3,17 @@ package forms
 import forms.validation.{DateWithDaysValidator, DateWithDaysValues, FieldError, FieldHint}
 import models.Question
 import org.joda.time.format._
+import org.joda.time.LocalDate
 import play.api.Logger
 import play.twirl.api.Html
 
 case class DateWithDaysField(name: String, validator: DateWithDaysValidator) extends Field {
 
-  val dateField = DateField(s"$name.date")
+  val dateField = DateField(s"$name.date", validator.dateValidator)
   val daysField = TextField(Some("Days"), s"$name.days", isNumeric = true)
 
   val fmt = DateTimeFormat.forPattern("d MMMM yyyy")
+  val accessFmt = AccessibleDateTimeFormat()
 
   override def renderFormInput(questions: Map[String, Question], answers: Map[String, String], errs: Seq[FieldError], hints: Seq[FieldHint]): Html =
     views.html.renderers.dateWithDaysField(this, questions, answers, errs, hints)
@@ -25,11 +27,26 @@ case class DateWithDaysField(name: String, validator: DateWithDaysValidator) ext
     val vs = DateWithDaysValues(Some(DateValues(day, month, year)), answers.get(s"${daysField.name}"))
     validator.validate("", vs).map { dwd =>
       val endDate = dwd.date.plusDays(dwd.days - 1)
-      views.html.renderers.preview.dateWithDaysField(this, fmt.print(dwd.date), dwd.days, fmt.print(endDate))
+      views.html.renderers.preview.dateWithDaysField(this, fmt.print(dwd.date), dwd.days, fmt.print(endDate), accessFmt.print(dwd.date), accessFmt.print(endDate))
     }.leftMap { errs =>
       Logger.debug(errs.toString())
       // TODO: we rely on only being called with valid answers, but what if they're not?
-      views.html.renderers.preview.dateWithDaysField(this, "", 0, "")
+      views.html.renderers.preview.dateWithDaysField(this, "", 0, "", "", "")
     }.fold(identity, identity)
+  }
+}
+
+case class AccessibleDateTimeFormat() {
+  val inner = DateTimeFormat.forPattern("MMMM yyyy");
+
+  def print(date: LocalDate): String = {
+    val x = (date.getDayOfMonth) match {
+      case n if Seq(11, 12, 13) contains n => n+"th"
+      case n if n%10 == 1 => n+"st"
+      case n if n%10 == 2 => n+"nd"
+      case n if n%10 == 3 => n+"rd"
+      case n => n+"th"
+    }
+    s"$x of ${inner.print(date)}"
   }
 }
