5637a8d2f057acb6feacaf96d8de0c70aca4bb13
Doug Clinton <doug@wellfactored.com>
2016-11-23 15:46:56 +0000

    Expanded the unit tests for DateTimeRangeValidator

--- src/test/scala/forms/validation/DateTimeRangeValidatorTest.scala
+++ src/test/scala/forms/validation/DateTimeRangeValidatorTest.scala
@@ -7,26 +7,33 @@ import org.scalatest.{Matchers, OptionValues, WordSpecLike}
 class DateTimeRangeValidatorTest extends WordSpecLike with Matchers with OptionValues {
   val validDateValues1 = Some(DateValues(Some("31"), Some("1"), Some("2017")))
   val validDateValues2 = Some(DateValues(Some("31"), Some("3"), Some("2017")))
+  val path = "dates"
 
   "validate" should {
     val validator = DateTimeRangeValidator(allowPast = true, isEndDateMandatory = true)
-    "succeed if both start and end dates are supplied and are valid" in {
+    "succeed if both start and end " + path + " are supplied and are valid" in {
       val values = DateTimeRangeValues(validDateValues1, validDateValues2, None)
-
-      validator.validate("dates", values) shouldBe a[Valid[_]]
+      validator.validate(path, values) shouldBe a[Valid[_]]
     }
 
-    "fail if the start date is not provided" in {
+    "fail with three startDate errors if the start date is not provided" in {
       val values = DateTimeRangeValues(None, validDateValues2, None)
-
-      validator.validate("dates", values) shouldBe a[Invalid[_]]
+      val result = validator.validate(path, values)
+      result shouldBe a[Invalid[_]]
+      result.leftMap { errs =>
+        errs.tail.length shouldBe 2
+        errs.map(_.path should startWith(s"$path.startDate"))
+      }
     }
 
-    "fail if the end date is before the start date" in {
+    "fail with a single error if the end date is before the start date" in {
       val values = DateTimeRangeValues(validDateValues2, validDateValues1, None)
-
-      val v = validator.validate("dates", values)
-      v shouldBe a[Invalid[_]]
+      val result = validator.validate(path, values)
+      result shouldBe a[Invalid[_]]
+      result.leftMap { errs =>
+        errs.tail.length shouldBe 0
+        errs.head.path shouldBe path
+      }
     }
   }
 
