b83ad64c4a9fd1b1f569e07f67c6a1603015d6c9
Doug Clinton <doug@wellfactored.com>
2016-11-25 10:08:59 +0000

    Added further date range validation to check that the end date is present if the "supplied" flag from the form is set.

--- src/main/scala/forms/validation/DateTimeRangeValidator.scala
+++ src/main/scala/forms/validation/DateTimeRangeValidator.scala
@@ -19,38 +19,67 @@ case class DateTimeRange(startDate: LocalDate, endDate: Option[LocalDate])
 case class DateTimeRangeValidator(allowPast: Boolean, isEndDateMandatory: Boolean) extends FieldValidator[DateTimeRangeValues, DateTimeRange] {
   val dateValidator = DateFieldValidator(allowPast)
 
-  val mandatoryEndDateIsPresentV = new FieldValidator[DateTimeRangeValues, DateTimeRangeValues] {
-    override def validate(path: String, vs: DateTimeRangeValues): ValidatedNel[FieldError, DateTimeRangeValues] = {
-      (isEndDateMandatory, vs.endDate) match {
-        case (true, None) => FieldError(path, "End date must be supplied").invalidNel
-        case _ => vs.valid
-      }
-    }
-  }
+  val mustProvideValidEndDateMessage = "You must provide a valid end date"
+  val endMustBeLaterThanStartMessage = "End date must be later than the start date"
 
-  val fieldLevelV = new FieldValidator[DateTimeRangeValues, DateTimeRange] {
+  /**
+    * Check that the individual form values are valid and build up a `DateTimeRange` instance that
+    * can be further validated.
+    */
+  val fieldLevelValidations = new FieldValidator[DateTimeRangeValues, DateTimeRange] {
     override def validate(path: String, vs: DateTimeRangeValues): ValidatedNel[FieldError, DateTimeRange] = {
       val sdv: DateValues = vs.startDate.getOrElse(DateValues(None, None, None))
       val edv: Option[DateValues] = vs.endDate
 
       val startDateV = dateValidator.validate(s"$path.startDate", sdv)
-      val endDateV = edv.map(dateValidator.validate(s"$path.endDate", _).map(Some(_))).getOrElse(None.valid)
-      val mandatoryV = mandatoryEndDateIsPresentV.validate(path, vs)
+      // First check that the end date is valid if it's present
+      val endDateValid = edv.map(dateValidator.validate(s"$path.endDate", _).map(Some(_))).getOrElse(None.valid)
+      // And then check if it's present if the `endDateProvided` flag is set
+      val endDateV = endDateValid.map(od => (od, vs.endDateProvided.getOrElse(false))).andThen(endDateIsPresentIfSupplied.validate(path, _))
 
-      (mandatoryV |@| startDateV |@| endDateV).map((_, sd, ev) => DateTimeRange.apply(sd, ev))
+      (startDateV |@| endDateV).map(DateTimeRange.apply)
     }
   }
 
-  val startDateBeforeEndDateV = new FieldValidator[DateTimeRange, DateTimeRange] {
+  /**
+    * Check that if the `isEndDateMandatory` flag is `true` that an end date is present on the form. This is
+    * irrespective of the value of `endDateProvided`.
+    */
+  val endDateIsPresentIfMandatory = new FieldValidator[DateTimeRange, DateTimeRange] {
+    override def validate(path: String, vs: DateTimeRange): ValidatedNel[FieldError, DateTimeRange] = {
+      (isEndDateMandatory, vs.endDate) match {
+        case (true, None) =>
+
+          FieldError(path, mustProvideValidEndDateMessage).invalidNel
+        case _ => vs.valid
+      }
+    }
+  }
+
+  /**
+    * Check that, if the form values indicate that the user has said they're providing an end date,
+    * then the user has actually provided values for the end date.
+    */
+  val endDateIsPresentIfSupplied = new FieldValidator[(Option[LocalDate], Boolean), Option[LocalDate]] {
+    override def validate(path: String, vs: (Option[LocalDate], Boolean)) = {
+      vs match {
+        case (None, true) => FieldError(path, mustProvideValidEndDateMessage).invalidNel
+        case _ => vs._1.valid
+      }
+    }
+  }
+
+  val startDateIsBeforeEndDate = new FieldValidator[DateTimeRange, DateTimeRange] {
     override def validate(path: String, dtr: DateTimeRange): ValidatedNel[FieldError, DateTimeRange] = {
       dtr.endDate.map(_.isAfter(dtr.startDate)) match {
-        case Some(false) => FieldError(path, "End date must be later than the start date").invalidNel
+        case Some(false) =>
+          FieldError(path, endMustBeLaterThanStartMessage).invalidNel
         case _ => dtr.valid
       }
     }
   }
 
   override def validate(path: String, vs: DateTimeRangeValues): ValidatedNel[FieldError, DateTimeRange] = {
-    (fieldLevelV andThen startDateBeforeEndDateV).validate(path, vs)
+    (fieldLevelValidations andThen endDateIsPresentIfMandatory andThen startDateIsBeforeEndDate).validate(path, vs)
   }
 }
