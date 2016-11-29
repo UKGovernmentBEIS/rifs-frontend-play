46c654910fda8653357f4009afa08c1d6273a3a9
Doug Clinton <doug@wiredthing.com>
2016-11-29 11:58:34 +0000

    Added Reads for CostItemField to correspond to a change in the data in the database

--- src/main/scala/fields/FieldReads.scala
+++ src/main/scala/fields/FieldReads.scala
@@ -12,7 +12,6 @@ object FieldReads {
   implicit val textAreaFieldReads = Json.reads[TextAreaField]
   implicit val dateWithDaysReads = Json.reads[DateWithDaysField]
   implicit val dateTimeRangeReads = Json.reads[DateTimeRangeField]
-  implicit val costListReads = Json.reads[CostListField]
   implicit val costItemReads = Json.reads[CostItemField]
 
   implicit object fieldReads extends Reads[Field] {
@@ -23,7 +22,6 @@ object FieldReads {
           case "textArea" => json.validate[TextAreaField]
           case "dateWithDays" => json.validate[DateWithDaysField]
           case "dateTimeRange" => json.validate[DateTimeRangeField]
-          case "costList" => json.validate[CostListField]
           case "costItem" => json.validate[CostItemField]
           case t => JsError(s"unknown field type $t")
         }
