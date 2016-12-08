24a1ac84305a4587e4aeb20f37afd200ff047599
Doug Clinton <doug@wiredthing.com>
2016-12-08 22:04:14 +0000

    Fixed classname and extracted out a function to build the test Opportunity object

--- src/test/scala/models/OpportunityTest.scala
+++ src/test/scala/models/OpportunityTest.scala
@@ -3,56 +3,56 @@ package models
 import org.joda.time.{DateTime, LocalDate}
 import org.scalatest.{Matchers, OptionValues, WordSpecLike}
 
-class OpertunityTest  extends WordSpecLike with Matchers with OptionValues {
+class OpportunityTest extends WordSpecLike with Matchers with OptionValues {
   val testDateBeforeCurrent = LocalDate.now.minusMonths(1)
   val testDateAfterCurrent = LocalDate.now.plusMonths(1)
   val testDateCurrent = LocalDate.now
   val publishedDate: DateTime = DateTime.now.minusMonths(1)
 
+  def opp(startDate: LocalDate, endDate: Option[LocalDate], publishedAt: Option[DateTime]) = Opportunity(OpportunityId(1), "Test opportunity", startDate, endDate, OpportunityValue(2000.00, "spondulix"), publishedAt, None, Seq())
+
   "validate" when {
     "Opportunity is published and starts before current date " should {
       "show as status Open" in {
-        val o: Opportunity = Opportunity (OpportunityId(1), "Test opportunity", testDateBeforeCurrent, None, OpportunityValue(2000.00, "spondulix"), Some(publishedDate), None, Seq())
+        val o: Opportunity = opp(testDateBeforeCurrent, None, Some(publishedDate))
         o.statusString shouldBe "Open"
       }
     }
 
     "Opportunity is published and starts on current date " should {
       "show as status Open" in {
-        val o: Opportunity = Opportunity (OpportunityId(1), "Test opportunity", testDateCurrent, None, OpportunityValue(2000.00, "spondulix"), Some(publishedDate), None, Seq())
+        val o: Opportunity = opp(testDateCurrent, None, Some(publishedDate))
         o.statusString shouldBe "Open"
       }
     }
 
     "Opportunity is published and starts before current date and ends before end date" should {
       "show as status Open" in {
-        val o: Opportunity = Opportunity (OpportunityId(1), "Test opportunity", testDateBeforeCurrent, Some(testDateAfterCurrent), OpportunityValue(2000.00, "spondulix"), Some(publishedDate), None, Seq())
+        val o: Opportunity = opp(testDateBeforeCurrent, Some(testDateAfterCurrent), Some(publishedDate))
         o.statusString shouldBe "Open"
       }
     }
 
     "Opportunity is published and starts after current date" should {
       "show as status Queued" in {
-        val o: Opportunity = Opportunity(OpportunityId(1), "Test opportunity", testDateAfterCurrent, None, OpportunityValue(2000.00, "spondulix"), Some(publishedDate), None, Seq())
+        val o: Opportunity = opp(testDateAfterCurrent, None, Some(publishedDate))
         o.statusString shouldBe "Queued"
       }
     }
 
     "Opportunity is published and ends before current date" should {
       "show as status Closed" in {
-        val o: Opportunity = Opportunity (OpportunityId(1), "Test opportunity", testDateBeforeCurrent, Some(testDateBeforeCurrent), OpportunityValue(2000.00, "spondulix"), Some(publishedDate), None, Seq())
+        val o: Opportunity = opp(testDateBeforeCurrent, Some(testDateBeforeCurrent), Some(publishedDate))
         o.statusString shouldBe "Closed"
       }
     }
 
     "Opportunity is not published" should {
       "show as status Draft" in {
-        val o: Opportunity = Opportunity (OpportunityId(1), "Test opportunity", testDateBeforeCurrent, Some(testDateAfterCurrent), OpportunityValue(2000.00, "spondulix"), None, None, Seq())
+        val o: Opportunity = opp(testDateBeforeCurrent, Some(testDateAfterCurrent), None)
         o.statusString shouldBe "Draft"
       }
     }
-
   }
-
 }
 
