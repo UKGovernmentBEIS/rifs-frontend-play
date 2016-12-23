2911d0e476372e54e83ee983d9fd11ae3e9c6a91
Doug Clinton <doug@wellfactored.com>
2016-12-23 18:12:06 +0000

    Generated copyright headers on all scala files (using sbt-header plugin)

--- src/main/scala/controllers/ActionHandler.scala
+++ src/main/scala/controllers/ActionHandler.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2016  Department for Business, Energy and Industrial Strategy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package controllers
 
 import javax.inject.Inject
@@ -19,11 +36,10 @@ class ActionHandler @Inject()(applications: ApplicationOps, applicationForms: Ap
 
   def doSave(app: ApplicationSectionDetail, fieldValues: JsObject): Future[Result] = {
     app.formSection.sectionType match {
-      case SectionTypeForm =>
-        JsonHelpers.allFieldsEmpty(fieldValues) match {
-          case true => applications.deleteSection(app.id, app.sectionNumber).map(_ => redirectToOverview(app.id))
-          case false => applications.saveSection(app.id, app.sectionNumber, fieldValues).map(_ => redirectToOverview(app.id))
-        }
+      case SectionTypeForm => {
+        if (JsonHelpers.allFieldsEmpty(fieldValues)) applications.deleteSection(app.id, app.sectionNumber)
+        else applications.saveSection(app.id, app.sectionNumber, fieldValues)
+      }.map(_ => redirectToOverview(app.id))
       case SectionTypeList => Future.successful(redirectToOverview(app.id))
     }
   }
